4a51e631c9752d64c731431b9404f90e9b1ed1be
DOO\Kevin.MunSH <mshou17194@hotmail.com>
2024-11-05 22:55:46 +0800

    First Draft Upload (Basic Structure)

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+// src/components/About.js
+import React from 'react';
+import './About.css';
+
+const About = () => (
+  <section id="about" className="about">
+    <h2>About Me</h2>
+    <p>Hello! I’m a software developer with a passion for building web applications and solving complex problems.</p>
+  </section>
+);
+
+export default About;
